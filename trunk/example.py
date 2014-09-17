42e62aeec9a3aff51bf4f4106d6eb67c2c3d231f
mfschubert@gmail.com <mfschubert@gmail.com@3cc822f7-8939-fc73-40b9-0dfa2b7d03a2>
2014-09-17 22:41:33 +0000

    Modified basic example, calculates the band structure of a quantum well along with the wavefunctions.

--- trunk/example.py
+++ trunk/example.py
@@ -1,37 +1,36 @@
 from ledsim import *
 import material, solve
 
-# Choose the material for the structure
+# Choose the material for the structure.
 mat = material.AlGaInN()
 
-# Define the layer stack
-layers = [Layer(mat,thickness=100e-9,x=0.0,y=0.0,Nd=4e18*1e6),
-          Layer(mat,thickness=10e-9 ,x=0.0,y=0.0,Nd=1e18*1e6),
-          Layer(mat,thickness=3e-9  ,x=0.0,y=0.1),
-          Layer(mat,thickness=10e-9 ,x=0.0,y=0.0,Nd=1e18*1e6),
-          Layer(mat,thickness=3e-9  ,x=0.0,y=0.1),
-          Layer(mat,thickness=10e-9 ,x=0.0,y=0.0,Nd=1e18*1e6),
-          Layer(mat,thickness=3e-9  ,x=0.0,y=0.1),
-          Layer(mat,thickness=10e-9 ,x=0.0,y=0.0,Nd=1e18*1e6),
-          Layer(mat,thickness=3e-9  ,x=0.0,y=0.1),
-          Layer(mat,thickness=10e-9 ,x=0.0,y=0.0),
-          Layer(mat,thickness=3e-9  ,x=0.0,y=0.1),
-          Layer(mat,thickness=10e-9 ,x=0.0,y=0.0),
-          Layer(mat,thickness=30e-9 ,x=0.1,y=0.0,Na=4e19*1e6),
-          Layer(mat,thickness=100e-9,x=0.0,y=0.0,Na=4e19*1e6)]
+# Define some layers used in the LED active region.
+nGaN   = Layer(material=mat,thickness=100e-9,x=0.00,y=0.00,Nd=4e18*1e6)
+bar    = Layer(material=mat,thickness=4.5e-9,x=0.00,y=0.00,isQuantum=True)
+qw     = Layer(material=mat,thickness=3.0e-9,x=0.00,y=0.12,isQuantum=True)
+spacer = Layer(material=mat,thickness=10.e-9,x=0.00,y=0.00,isQuantum=True)
+ebl    = Layer(material=mat,thickness=30.e-9,x=0.10,y=0.00,Na=4e19*1e6)
+pGaN   = Layer(material=mat,thickness=100e-9,x=0.00,y=0.00,Na=4e19*1e6)
 
-so = solve.SolverOpts(verboseLevel=3)
+# Define the layer stack.
+# layers = [nGaN]+[bar,qw]*3+[spacer,ebl,pGaN]
+layers = [bar,qw,bar]
+# layers = [qw]
 
+# Build the structure
+gridOpts    = GridOpts(dzQuantum=1e-10)
+modelOpts   = ModelOpts(quantum=True)
+quantumOpts = QuantumOpts(boundaryType='periodic')
+s = build(layers,gridOpts=gridOpts,modelOpts=modelOpts,quantumOpts=quantumOpts)
 
-s  = build(layers)
+# Solve for two conditions, the local equilibrium and the condition at 35A/cm2
+solverOpts = SolverOpts(verboseLevel=4)
+c1 = solve.solve_equilibrium_local(s,solverOpts=solverOpts)
+c2 = solve.solve_poisson_periodic(c1,Ntarget=1e17*1e6*1e-9,solverOpts=solverOpts)
 
-c1 = solve.solve_equilibrium_local(s)
-c2 = solve.bias(c1,Jtarget=100*1e4)
 pylab.ion()
-
 c2.plot('bands',figNum=1)
 c2.plot('carriers',figNum=2)
-c2.plot('recombination',figNum=3)
-c2.plot('currents',figNum=4)
-
+c2.plot('EcWavefunctions',figNum=3)
+c2.plot('EvWavefunctions',figNum=4)
 pylab.show()
